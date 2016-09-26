3844ba1bee70462f6232aebfcd32f35737896506
Kelly de Vries <kdv@me.com>
2016-09-26 11:49:18 -0700

    merge header and container components

--- scripts/App.js
+++ scripts/App.js
@@ -1,12 +1,13 @@
 import React, {Component} from 'react';
+import Header             from './components/header.jsx';
 import Container from  './components/container.jsx';
 
 export default class App extends Component {
   render() {
     return (
       <div>
-      <h1>Hello, Abeer!</h1>
-      <Container />
+        <Header />
+        <Container />
       </div>
     );
   }
