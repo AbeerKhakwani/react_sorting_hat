9ffcfa0884be2448407f89417d762e4815da1ef2
Kelly de Vries <kdv@me.com>
2016-09-26 14:30:51 -0700

    add mapping through src props for images

--- scripts/components/container.jsx
+++ scripts/components/container.jsx
@@ -5,42 +5,37 @@ import Tile               from './tile.jsx';
 export default class Container extends Component {
 
   render() {
-    // let imgArray = ["img/owl","img/baby.png",
-    //               "img/bellpepper.png",
-    //               "img/duck.png",
-    //               "img/flag.png",
-    //               "img/globe.png",
-    //               "img/lighter.png",
-    //               "img/pretzel.png",
-    //               "img/rubberduck.png",
-    //               "img/suitcase.png",
-    //               "img/teeth.png",
-    //               "img/tomatoes.png",
-    //               "img/camera.png",
-    //               "img/pliers.png",
-    //               "img/flowerpot.png",
-    //               "img/pig.png",
-    //               "img/pigeon.png",
-    //               "img/lemon.png",
-    //               "img/hammer.png",
-    //               "img/glove.png"]
+    let imgArray = ["img/owl","img/baby.png",
+                  "img/bellpepper.png",
+                  "img/duck.png",
+                  "img/flag.png",
+                  "img/globe.png",
+                  "img/lighter.png",
+                  "img/pretzel.png",
+                  "img/rubberduck.png",
+                  "img/suitcase.png",
+                  "img/teeth.png",
+                  "img/tomatoes.png",
+                  "img/camera.png",
+                  "img/pliers.png",
+                  "img/flowerpot.png",
+                  "img/pig.png",
+                  "img/pigeon.png",
+                  "img/lemon.png",
+                  "img/hammer.png",
+                  "img/glove.png"]
 
   const containerStyle ={
-    display: 'flex', /* or inline-flex */
+    display: 'flex',
     backgroundColor: 'yellow',
     flexWrap: 'wrap'
   }
     return (
-      // Add your component markup and other subcomponent references here.
+
       <div style={containerStyle}>
-          <Tile src="../img/globe.png" name="kelly"/>
-          <Tile src="../img/globe.png" name="kelly"/>
-          <Tile src="../img/globe.png" name="kelly"/>
-          <Tile src="../img/globe.png" name="kelly"/>
-          <Tile src="../img/globe.png" name="kelly"/>
-          <Tile src="../img/globe.png" name="kelly"/>
-          <Tile src="../img/globe.png" name="kelly"/>
-          <Tile src="../img/globe.png" name="kelly"/>
+        {imgArray.map(function(src, index){
+          return <Tile src={src} name="kelly"/>;
+        })}
       </div>
     );
   }
